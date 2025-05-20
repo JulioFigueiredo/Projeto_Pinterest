b7ea0091ba81e167b34529dfcdc302e98c856d4a
JulioFigueiredo <julioo.1amorim@gmail.com>
2025-05-20 14:49:35 -0300

    feat: add routes and dinamic pages

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+
+# url_for permite acessar o endpoint pelo nome da função
 
 app = Flask(__name__)
 
@@ -6,9 +8,9 @@ app = Flask(__name__)
 def homepage():
     return render_template("homepage.html")
 
-@app.route("/perfil")
-def perfil():
-    return render_template("perfil.html")
+@app.route("/perfil/<usuario>") # <> mostra que é uma variável
+def perfil(usuario): # variável é passada como parâmetro
+    return render_template("perfil.html", usuario=usuario)
 
 if __name__ == "__main__":
     app.run(debug=True)
