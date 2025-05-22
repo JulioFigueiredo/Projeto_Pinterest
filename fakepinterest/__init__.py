50d64316ba541fc080b1ef7b84ac9ae5e45c7bad
JulioFigueiredo <julioo.1amorim@gmail.com>
2025-05-22 14:40:53 -0300

    feat: start to implement login and security

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
-
+app.config["SECRET_KEY"] = ""
 
 database = SQLAlchemy(app)
+bcrypt = Bcrypt(app) # criptografia
+login_manager = LoginManager(app) # login
+login_manager.login_view = "homepage" # Login será feito em homepage
 
 from fakepinterest import routes
--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -1,12 +1,13 @@
 from flask import render_template, url_for
 from fakepinterest import app
 # url_for permite acessar o endpoint pelo nome da função
-
+from flask_login import login_required # exige que o usuário tenha login
 
 @app.route("/") # caminho com link do site
 def homepage():
     return render_template("homepage.html")
 
 @app.route("/perfil/<usuario>") # <> mostra que é uma variável
+@login_required
 def perfil(usuario): # variável é passada como parâmetro
     return render_template("perfil.html", usuario=usuario)
