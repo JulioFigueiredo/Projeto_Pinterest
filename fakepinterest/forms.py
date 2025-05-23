e16821b0ee2c774351d6f281c938828e64fcf383
JulioFigueiredo <julioo.1amorim@gmail.com>
2025-05-23 11:27:17 -0300

    feat: add forms class and start validation

---
+++ fakepinterest/forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from fakepinterest.models import Usuario
+
+
+class FormLogin(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    senha = PasswordField("Senha", validators=[DataRequired()])
+    botao_confirmacao = SubmitField("Fazer login")
+
+class FormCriarConta(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    username = StringField("Username", validators=[DataRequired()])
+    senha = PasswordField("Senha", validators=[DataRequired, Length(6,20)])
+    confirmacao_senha = PasswordField("Confirmação de senha", validators=[DataRequired, EqualTo("senha")])
+    botao_confirmacao = SubmitField("Criar Conta")
+    
+    def validate_email(self, email): # Validar email
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario: # confere se já existe um usuário
+            return ValidationError("Email já cadastrado, faça login para se cadastrar")
