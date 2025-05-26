b3be9c3d27d132fa646abf7714762d7343ac8134
Júlio Figueiredo <julioo.1amorim@gmail.com>
2025-05-26 19:59:46 -0300

    feat: adding user to db and encrypting password

--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -1,8 +1,10 @@
 from flask import render_template, url_for
-from fakepinterest import app
+from fakepinterest.models import Usuario
+from fakepinterest import app, database, bcrypt
 # url_for permite acessar o endpoint pelo nome da função
 from flask_login import login_required # exige que o usuário tenha login
 from fakepinterest.forms import FormLogin, FormCriarConta
+from fakepinterest.models import Usuario, Foto
 
 @app.route("/", methods=["GET", "POST"]) # caminho com link do site
 def homepage():
@@ -11,8 +13,13 @@ def homepage():
 
 @app.route("/criarconta", methods=["GET","POST"])
 def criarconta():
-    formcriarconta = FormCriarConta()
-    return render_template("criarconta.html", form=formcriarconta)
+    form_criarconta = FormCriarConta()
+    if form_criarconta.validate_on_submit():
+        senha = bcrypt.generate_password_hash(form_criarconta.senha.data) # vai criptografar a senha 
+        usuario = Usuario(username=form_criarconta.username.data, email=form_criarconta.email.data, senha=senha)
+        database.session.add(usuario) # adiciona o usuário no db
+        database.session.commit()
+    return render_template("criarconta.html", form=form_criarconta)
 
 
 @app.route("/perfil/<usuario>") # <> mostra que é uma variável
