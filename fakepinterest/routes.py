c9e3d3071ac3cd0e0068553a8612e248f3febc2a
JulioFigueiredo <julioo.1amorim@gmail.com>
2025-05-26 10:57:51 -0300

    feat: add GET and POST methods in routes

--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -2,10 +2,18 @@ from flask import render_template, url_for
 from fakepinterest import app
 # url_for permite acessar o endpoint pelo nome da função
 from flask_login import login_required # exige que o usuário tenha login
+from fakepinterest.forms import FormLogin, FormCriarConta
 
-@app.route("/") # caminho com link do site
+@app.route("/", methods=["GET", "POST"]) # caminho com link do site
 def homepage():
-    return render_template("homepage.html")
+    formLogin = FormLogin()
+    return render_template("homepage.html", form=formLogin)
+
+@app.route("/criarconta", methods=["GET","POST"])
+def criarconta():
+    formcriarconta = FormCriarConta()
+    return render_template("criarconta.html", form=formcriarconta)
+
 
 @app.route("/perfil/<usuario>") # <> mostra que é uma variável
 @login_required
