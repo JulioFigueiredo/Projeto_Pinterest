90c078e4c8cbdfb360c138c80b2431510ecbcc98
JulioFigueiredo <julioo.1amorim@gmail.com>
2025-05-26 10:56:51 -0300

    fix: fix error in password confirmation button

--- fakepinterest/forms.py
+++ fakepinterest/forms.py
@@ -3,7 +3,6 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
 from fakepinterest.models import Usuario
 
-
 class FormLogin(FlaskForm):
     email = StringField("E-mail", validators=[DataRequired(), Email()])
     senha = PasswordField("Senha", validators=[DataRequired()])
@@ -12,11 +11,11 @@ class FormLogin(FlaskForm):
 class FormCriarConta(FlaskForm):
     email = StringField("E-mail", validators=[DataRequired(), Email()])
     username = StringField("Username", validators=[DataRequired()])
-    senha = PasswordField("Senha", validators=[DataRequired, Length(6,20)])
-    confirmacao_senha = PasswordField("Confirmação de senha", validators=[DataRequired, EqualTo("senha")])
+    senha = PasswordField("Senha", validators=[DataRequired(), Length(min=6, max=20)])
+    confirmacao_senha = PasswordField("Confirmação de senha", validators=[DataRequired(), EqualTo("senha")])
     botao_confirmacao = SubmitField("Criar Conta")
-    
-    def validate_email(self, email): # Validar email
+
+    def validate_email(self, email):
         usuario = Usuario.query.filter_by(email=email.data).first()
-        if usuario: # confere se já existe um usuário
-            return ValidationError("Email já cadastrado, faça login para se cadastrar")
+        if usuario:
+            raise ValidationError("Email já cadastrado, faça login para se cadastrar")
