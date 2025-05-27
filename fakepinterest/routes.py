74a49f174b15e5df4f843538c7a3988340f63389
Júlio Figueiredo <julioo.1amorim@gmail.com>
2025-05-27 10:34:43 -0300

    feat: implement sign-in, sign-up and logout of users

--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -1,14 +1,19 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect
 from fakepinterest.models import Usuario
 from fakepinterest import app, database, bcrypt
 # url_for permite acessar o endpoint pelo nome da função
-from flask_login import login_required # exige que o usuário tenha login
+from flask_login import login_required, login_user, logout_user, current_user
 from fakepinterest.forms import FormLogin, FormCriarConta
 from fakepinterest.models import Usuario, Foto
 
 @app.route("/", methods=["GET", "POST"]) # caminho com link do site
 def homepage():
     formLogin = FormLogin()
+    if formLogin.validate_on_submit():
+        usuario = Usuario.query.filter_by(email=formLogin.email.data).first() # procura o usuário por email
+        if usuario and bcrypt.check_password_hash(usuario.senha, formLogin.senha.data): # verifica se existe o usuario e a senha corresponde a senha criptografada
+            login_user(usuario)
+            return redirect(url_for("perfil", usuario=usuario.username))
     return render_template("homepage.html", form=formLogin)
 
 @app.route("/criarconta", methods=["GET","POST"])
@@ -19,6 +24,8 @@ def criarconta():
         usuario = Usuario(username=form_criarconta.username.data, email=form_criarconta.email.data, senha=senha)
         database.session.add(usuario) # adiciona o usuário no db
         database.session.commit()
+        login_user(usuario, remember=True)
+        return redirect(url_for("perfil", usuario=usuario.username)) # redireciona para a função perfil a partir do username
     return render_template("criarconta.html", form=form_criarconta)
 
 
@@ -26,3 +33,9 @@ def criarconta():
 @login_required
 def perfil(usuario): # variável é passada como parâmetro
     return render_template("perfil.html", usuario=usuario)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user() #  desloga o usuário atual
+    return redirect(url_for("homepage"))
